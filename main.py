6665ee5230a7669708da3479b916d3908424a1b7
march <marchmarch0322@gmail.com>
2021-06-22 13:16:12 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import time
+
+st.title('streamlit 超入門')
+
+st.write('ブレグレスバーの表示')
+'Start!!'
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.1)
+
+'Done!!!!'
+
+
+
+left_column, right_column = st.beta_columns(2)
+button = left_column.button('右のカラムの文字を表示')
+if button:
+    right_column.write('ここは右カラム')
+
+expander =  st.beta_expander('問い合わせ')
+expander.write('問い合わせ内容を書く')
+#if st.checkbox('show Image'):
+    #img = Image.open('test.jpeg')
+    #st.image(img, caption='rena', use_column_width=True)
+ 
+# text = st.text_input('あなた趣味をを教えてください。',)  
+
+# 'あなたの趣味',text,
+
+# condition = st.slider('あなたの今の調子は？',0, 100, 50)
+# 'コンディション：',condition
